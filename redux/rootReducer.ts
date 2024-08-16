f4adcf65fd12a849f41dac03477e625c5ef06d56
vietnguyen <vietnguyen@fortna.com>
2024-08-16 11:31:36 +0700

    reset codebase

--- redux/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit'
-import counterReducer from './counter/counterSlice'
-
-const rootReducer = combineReducers({
-  counter: counterReducer,
-})
-
-export default rootReducer
